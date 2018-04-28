688446ff1f8514ef5f514c22cec492a6ae158300
Eze Peter U <pee2472003@yahoo.com>
2018-04-28 18:43:46 +1000

    Added class_lib for OOP

---
+++ classes/class_lib.php
@@ -0,0 +1,41 @@
+<?php
+ class CourierUsers {
+	 //General user of the system. Could act as transporter, sender or receiver
+
+ }
+ 
+ class Journeys {
+	 //A transporter CourierUser indicates he will travel from one location to another on a given date 
+
+ }
+ 
+ class DeliveryItems{
+	 //A sender CourierUser lists items that he/she would want to be transported from one destination to another on a given date and time 
+	 
+ }
+ 
+ class InterestedTransporters{
+	 //For listed DeliveryItems, a transporter CourierUser indicates interest on which DeliveryItem to send and at what cost
+	 
+ }
+ 
+ class SelectedTransporters{
+	 //the sender CourierUser who listed DeliveryItem then chooses the best InterestedTransporter that he wants
+	 
+ }
+ 
+ class Payment{
+	 //after negotiations are finalised the sender CourierUser makes payment on the platform
+ }
+ 
+ class RateJourneys{
+	 // the sender CourierUser (or receiver CourierUser) rates the transporter CourierUser who handled the transported item
+ }
+ 
+ class Reports{
+	 //Handles all reports and queries about the system as well as the dashboard for each CourierUser
+ }
+ 
+ class UserManager{
+	 // This handles user account management for all CourierUsers
+ }
