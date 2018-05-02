e4af639ea5aa7b3f5585547d1f8c7f8159643498
tochukwu1991 <toscopearl@gmail.com>
2018-05-01 19:43:04 -0500

    I changed the design look of the login page

--- login.php
+++ login.php
@@ -2,39 +2,56 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Login Page</title>
+    <title></title>
+    <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class="container">
-      <div class="jumbotron jumbotron-fluid">
-       <div class="container">
-    <h1 class="display-4"><b>Log In</b></h1>
-    <p class="lead"><b>Enter your email and password to log in to CommCourier.</b></p>
-  </div>
+
+      <header>
+        <div class="imagelogo">
+          <img src="images/picturelogo.png" alt="">
+          <h6><i>Making money while journeying!!!</i></h6>
+        </div>
+      </header>
+
+      <div class="container"><br>
+    <div class="card" style="width: 45rem; height: 21rem; ">
+<div class="card-body">
+  <h2  style="text-align: center;"><b>Login In</b></h2>
+<br>
+<div class="form-group row">
+<label for="loginemail" class="col-sm-4 col-form-label">Email address</label>
+  <div class="col-sm-8">
+   <input type="email" class="form-control" id="loginemail" placeholder="" name="login email">
+</div>
+</div>
+
+<div class="form-group row">
+<label for="userpassword" class="col-sm-4 col-form-label">Password</label>
+  <div class="col-sm-8">
+   <input type="password" class="form-control" id="userpassword" placeholder="" name="user password">
+</div>
+</div>
+
+<div class="form-check">
+  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+  <label class="form-check-label" for="exampleCheck1">Keep me logged in</label>
 </div>
 
 <form action="process.php" method="post">
- <div class="form-group">
-   <label for="exampleInputEmail1">Email address</label>
-   <input type="email" class="form-control"  aria-describedby="emailHelp" placeholder="Enter email or Username" id="username" name="username">
-   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
- </div>
- <div class="form-group">
-   <label for="exampleInputPassword1">Password</label>
-   <input type="password" class="form-control"  placeholder="Password" id="pword" name="pword">
- </div>
- <div class="form-check">
-   <input type="checkbox" class="form-check-input" id="exampleCheck1">
-   <label class="form-check-label" for="exampleCheck1">Keep me logged in</label>
- </div>
- <button type="submit" class="btn btn-primary col-md-2" name="loginSubmitBtn">Submit</button>
- <button type="reset" class="btn btn-primary col-md-2">Reset</button>
+<button type="submit" class="btn btn-primary col-md-2" id="Submit" >Submit</button>
+<button type="reset" class="btn btn-secondary col-md-2" id="Reset">Cancel</button>
 </form>
 
+ </div>
 </div>
-  </body>
-</html>
+</div>
+
+
+      <footer>Copyright&copy;commcourier.com</footer>
+        </body>
+      </html>
