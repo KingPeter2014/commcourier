549b435ffc545d166cf80285a6bbcff44f246353
Eze Peter U <pee2472003@yahoo.com>
2018-05-06 18:40:21 +1000

    JQuery activated in homepage

--- Homepage.php
+++ Homepage.php
@@ -8,9 +8,18 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="jquery-3.3.1.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="scripts/pageloads.js"></script>
+<script src="scripts/jquery-3.3.1.min.js"></script> 
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="scripts/pageloads.js"></script>-->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#newjourney').on("click",function(){
+        $('#centralcontainer').load("listjourney.php");//listjourney.php
+        alert('List your new journey!');
+       }); 
+      
+     });
+  </script>
 
   </head>
   <body>
@@ -21,9 +30,17 @@
   ?>
 
 <!--  This section of the code creates the navbar containing the home, actors etc-->
+
      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <header>
+        <div class="imagelogo">
+        <img src="images/picturelogo.png" alt="">
+        <h6><i><font color="white">Making money while journeying!!!</font></i></h6>
+      </div>
+      </header>
+
       <div class="container"> 
-        <a class="navbar-brand" href="#">CommCOURIER</a> <div> <?php echo "<br/>Welcome:".$username;?></div>
+        <a class="navbar-brand" href="#">CommCOURIER</a> <div> </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -33,29 +50,30 @@
                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
              <li class="nav-item">
-                  <a class="nav-link" id="newjourney"  href="listjourney.php">New Journey</a> 
-                <!-- <a class="nav-link" id="newjourney"  href="#">New Journey</a>-->
+                 <!-- <a class="nav-link" id="newjourney"  href="listjourney.php">New Journey</a> -->
+                 <a class="nav-link" id="newjourney"  href="#">New Journey</a>
              </li>
 
              <li class="nav-item">
                 <a class="nav-link" href="submit-item.php">New Item</a>
              </li>
-			 <li class="nav-item">
-                <a class="nav-link" href="logout.php">Log out</a>
-             </li>
+			       
 
              <!-- This portion of the code creates the dropdown menu called Actors -->
              <li class="nav-item dropdown">
                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 Actors
+                 Find
                </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="http://localhost/commcourier/listjourney.php">Transporter</a>
-                <a class="dropdown-item" href="#">Sender</a>
+                <a class="dropdown-item" href="http://localhost/commcourier/listjourney.php">Journey</a>
+                <a class="dropdown-item" href="#">Item to Send</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Receiver if needed will be here </a>
+                <a class="dropdown-item" href="#">My Deals</a>
               </div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">Log out</a>
+             </li>
 			
           </ul>
 
@@ -70,8 +88,8 @@
 	
  
 
-     <div id="centralcontainer" style="top:15rem;" class="card" style="width: 45rem; height: 40rem; ">
-      <h1> <font color="red"> Use JQuery to load all contents to this div dynamically. Use $("#centralcontainer").load("filename.php");</font> </h1>
+     <div id="centralcontainer" style="top:5rem;" class="card" style="width: 45rem; height: 40rem; ">
+      <h1> <font color="red"> <?php echo "<br/>Welcome:".$username;?></font> </h1>
     </div>
 
    
--- scripts/pageloads.js
+++ scripts/pageloads.js
@@ -1,10 +1,9 @@
 <script type="text/javascript">
-		$(document).ready(function(){
-		   $("#newjourney").click(function(){
-		   	$("#centralcontainer").load("../listjourney.php");
-		   	alert("List your new journey!");
-		   }); 
- 
-		  
-		 });
-	</script>
+    $(document).ready(function(){
+        $('#newjourney').on("click",function(){
+        $('#centralcontainer').load("listjourney.php");//listjourney.php
+        alert('List your new journey!');
+       }); 
+      
+     });
+  </script>
