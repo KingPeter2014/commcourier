903f222e6ab415e5033af715125245a484b46618
Eze Peter U <pee2472003@yahoo.com>
2018-05-07 00:25:58 +1000

    edited homepage

--- Homepage.php
+++ Homepage.php
@@ -15,7 +15,6 @@
     $(document).ready(function(){
         $('#newjourney').on("click",function(){
         $('#centralcontainer').load("listjourney.php");//listjourney.php
-        alert('List your new journey!');
         //alert('List your new journey!');
        }); 
 
