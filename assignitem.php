f83a68cfbabc9cecd8ce6a23155f9dcf35731687
Eze Peter U <pee2472003@yahoo.com>
2018-05-18 17:20:14 +1000

    Assign.php now has header and footer

--- assignitem.php
+++ assignitem.php
@@ -1,12 +1,25 @@
 <?php if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
+
+
+	include("header.php");
+ ?>
+<div class="container">
+	<?php
     $username = $_SESSION['username'];
     $item = $_GET['item'];
     include("classes/class_lib.php");
     $bids = new InterestedTransporters();
 	$response = $bids->getBidsForItem($item);
 	echo $response;
+	?>
+
+</div>
+ <?php
+include("footer.php");
+?>
+
 
     
-    ?>
+   
